6f0ee6926331005deaf18331f23f8fea2a42b5ce
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-18 15:28:13 +0530

    [Added: Find Frequency of Elements]

---
+++ src/com/hashtable/HashTable.java
@@ -0,0 +1,28 @@
+package com.hashtable;
+
+import java.util.*;
+
+public class HashTable {
+
+	static void InputSentence(String userString) {
+		HashMap<String, Integer> frequency = new HashMap<String, Integer>();
+		String[] words = userString.split(" ");
+		for (String word : words) {
+			if (frequency.containsKey(word)) {
+				frequency.put(word, frequency.get(word) + 1);
+			} else {
+				frequency.put(word, 1);
+			}
+		}
+		Set<String> stringFrequency = frequency.keySet();
+		System.out.println("Frequency Of Words In Sentence: \n 'To Be Or Not To Be'");
+		for (String word : stringFrequency) {
+			System.out.println(word + " = " + frequency.get(word) + " times.");
+
+		}
+	}
+
+	public static void main(String[] args) {
+		InputSentence("To Be Or Not To Be");
+	}
+}
